d9a3c907ee8c26efddbc6c18818a48aaf6078b0b
kamakiri01 <kamakiri01@users.noreply.github.com>
2025-07-23 04:37:58 +0900

    add resolveSizeOptions test

--- spec/generateBitmapSpec.ts
+++ spec/generateBitmapSpec.ts
@@ -1,36 +1,34 @@
 import * as opentype from "opentype.js";
 import * as path from "path";
 //@ts-ignore
-import { calculateCanvasSize } from "../lib/generateBitmap";
-import type { GlyphRenderable, RenderableTable, ResolvedSizeOptions } from "../src/type";
-
-let font: opentype.Font;
-
-const resolvedSizeOptions = {
-    fixedWidth: undefined,
-    height: 13,
-    baselineHeight: 10.6,
-    margin: 1,
-    requiredHeight: 7.6,
-    lineHeight: 13,
-    descend: -3
-}
-
-const esolvedSizeOptionsFixed = JSON.parse(JSON.stringify(resolvedSizeOptions));
-esolvedSizeOptionsFixed.fixedWidth = 10;
-
-const abcdefghWdith = [
-	{ width: 7 }, // a
-	{ width: 8 }, // b
-	{ width: 7 }, // c
-	{ width: 8 }, // d
-	{ width: 7 }, // e
-	{ width: 7 }, // f
-	{ width: 8 }, // g
-	{ width: 8 }, // h
-];
+import { calculateCanvasSize, collectGlyphRenderables, resolveSizeOptions } from "../lib/generateBitmap";
+import type { BitmapFontEntryTable, GlyphRenderable, GlyphRenderableTable, RenderableTable, ResolvedSizeOptions, SizeOptions } from "../src/type";
 
 describe("calculateCanvasSize", function () {
+	const resolvedSizeOptions = {
+		fixedWidth: undefined,
+		height: 13,
+		baselineHeight: 10.6,
+		margin: 1,
+		requiredHeight: 7.6,
+		lineHeight: 13,
+		descend: -3
+	}
+
+	const esolvedSizeOptionsFixed = JSON.parse(JSON.stringify(resolvedSizeOptions));
+	esolvedSizeOptionsFixed.fixedWidth = 10;
+
+	const abcdefghWdith = [
+		{ width: 7 }, // a
+		{ width: 8 }, // b
+		{ width: 7 }, // c
+		{ width: 8 }, // d
+		{ width: 7 }, // e
+		{ width: 7 }, // f
+		{ width: 8 }, // g
+		{ width: 8 }, // h
+	];
+
 	it("no fixed", function () {
 		expect(calculateCanvasSize(abcdefghWdith.slice(0, 1), resolvedSizeOptions)).toEqual({ width: 16, height: 15 });
 		expect(calculateCanvasSize(abcdefghWdith.slice(0, 2), resolvedSizeOptions)).toEqual({ width: 32, height: 15 });
@@ -54,6 +52,30 @@ describe("calculateCanvasSize", function () {
 	});
 })
 
-describe("resolveSizeOptions", function () {
-	
+let font: opentype.Font;
+
+
+
+describe("resolveSizeOptions",function () {
+	it("", async function() {
+		font = await opentype.load(path.join(__dirname, "./fixtures/mplus-1c-light.ttf"));
+		const abcdefghWdith = "abcdefghWdith";
+		const entryTable = Array.from(abcdefghWdith).reduce((table, ch) => {
+			table[ch.charCodeAt(0)] = ch;
+			return table;
+		}, {} as BitmapFontEntryTable);
+
+		const glyphRenderableTable = collectGlyphRenderables(entryTable, font, { height: 13, margin: 1 } satisfies SizeOptions);
+		const resizedSizeOptions = resolveSizeOptions(glyphRenderableTable, {}, font);
+
+		expect(resizedSizeOptions).toEqual({
+		fixedWidth: undefined,
+		height: 13,
+		baselineHeight: 9.75,
+		margin: 1,
+		requiredHeight: 9.62,
+		lineHeight: 13,
+		descend: -0.13
+	});
+	})
 });
